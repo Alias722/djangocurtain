92717b4fdfe89e7dce7ffdcc452df1f350c65bb4
milliax <wei.sivir.pw>
2021-08-02 20:17:40 +0800

    Hello World

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const App = new express()
 
 App.get('/',(req,res)=>{
-	res.send('Hello World')
+	return res.send('Hello World')
 })
 
-App.listen('3000',()=>{console.log('server starting')})
+App.listen('8000',()=>{console.log('server starting')})
